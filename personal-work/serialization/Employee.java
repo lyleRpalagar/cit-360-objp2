e90051f51f447f7acc064cb7fe85b3b5ce9e6d21
lylerpalagar@gmail.com <lylerpalagar@gmail.com>
2015-05-25 20:06:37 -0600

    updated

---
+++ personal-work/serialization/Employee.java
@@ -0,0 +1,8 @@
+import java.io.Serializable;
+ 
+public class Employee implements Serializable
+{
+   public String firstName;
+   public String lastName;
+   private static final long serialVersionUID = 5462223600l;
+}
